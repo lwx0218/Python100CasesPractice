7c8cba10dea9b7432ad227020b8ed4067d31f4e7
lwx0218 <linzhen.luo@gmail.com>
2021-06-04 17:21:57 +0800

    Update p31.py

--- p31.py
+++ p31.py
@@ -7,3 +7,16 @@
 **程序分析：**这里用字典的形式直接将对照关系存好。
 '''
 
+
+weekT = {'u':'Tuesday','t':'Thursday'}
+weekS = {'a':'Saturday', 'u':'Sunday'}
+
+week = {'m':'Monday','w':'Wednesday','f':'Friday','t':weekT,'s':weekS}
+
+n = week[str(input("please enter the first letter: ")).lower()]
+
+if n == weekT or n ==weekS:
+    print(n[str(input("please enter the second the letter: ")).lower()])
+else:
+    print(n)
+
