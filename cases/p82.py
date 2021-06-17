d8e19cbddf81c0b096fe2b74a35ec44a910bb8c9
lwx0218 <linzhen.luo@gmail.com>
2021-06-17 17:51:25 +0800

    update

--- cases/p82.py
+++ cases/p82.py
@@ -1,5 +1,11 @@
 # coding: utf-8
 
 '''
-
+实例082：八进制转十进制
+**题目：**八进制转换为十进制
 '''
+
+print(int(input("please enter your number: "),8))
+
+n=eval('0o'+str(int(input('八进制输入：'))))
+print(n)
--- cases/p84.py
+++ cases/p84.py
@@ -1,5 +1,8 @@
 # coding: utf-8
 
 '''
-
+实例084：连接字符串
+**题目：**连接字符串。
 '''
+mylist = ['Brazil', 'Russia', 'India', 'China']
+print(','.join(mylist))
