8e797b36b98958d0f04660c7c46a9983f99e9b7f
lwx0218 <linzhen.luo@gmail.com>
2021-07-16 17:17:31 +0800

    Update p06.py

--- cases/p06.py
+++ cases/p06.py
@@ -6,12 +6,13 @@
 
 **程序分析：**斐波那契数列（Fibonacci sequence），从1,1开始，后面每一项等于前面两项之和。图方便就递归实现，图性能就用循环。
 '''
+from functools import lru_cache
 
-
+@lru_cache()
 def fib(n):
     return 1 if n <= 2 else fib(n - 1) + fib(n - 2)
 
-
+@lru_cache()
 def fib2(n):
     a, b = 1, 1
     for i in range(n - 1):
